fba664459e0f2ba38fe51bf53438e021069cf27d
Ella-42 <Ellakazam.h@gmail.com>
2024-03-26 19:16:26 +0100

    added some basic functionality to cub3d, can now close window, either by hitting the 'x' or escape, adding basic map parsing tomorrow(auto push :heart:)

--- cub3d.c
+++ cub3d.c
@@ -6,20 +6,64 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:24:34 by lpeeters          #+#    #+#             */
-/*   Updated: 2024/03/21 21:50:25 by lpeeters         ###   ########.fr       */
+/*   Updated: 2024/03/26 19:07:40 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-//parse map, handle errors, convert into interactive 2d video game
-int	main(void)
+// Print error messages
+int	prnt_err(char *str)
+{
+	return (ft_putendl_fd(str, 2), 1);
+}
+
+//exit in a clean way
+int	close_window(t_mlx *mlx, int status)
+{
+	mlx_destroy_window(mlx->ptr, mlx->win);
+	mlx_destroy_display(mlx->ptr);
+	free(mlx->ptr);
+	if (status == FAILURE)
+		exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
+	return (FAILURE);
+}
+
+/*
+	else if (keycode == UP || keycode == W)
+		move(mlx, mlx->map, UP);
+	else if (keycode == DOWN || keycode == S)
+		move(mlx, mlx->map, DOWN);
+	else if (keycode == LEFT || keycode == A)
+		move(mlx, mlx->map, LEFT);
+	else if (keycode == RIGHT || keycode == D)
+		move(mlx, mlx->map, RIGHT);
+	else if (keycode == M)
+		print_map(mlx->map);
+*/
+
+//defines what happens when certain keys are pressed
+int	key_event(int keycode, t_mlx *mlx)
+{
+	if (keycode == ESCAPE)
+		close_window(mlx, SUCCESS);
+	else
+		printf("Info: key not bound...\n");
+	return (0);
+}
+
+// Parse map, handle errors, convert into an interactive 3D video game
+int	main(int ac, char **av)
 {
 	t_mlx	mlx;
 
-	int test = ft_strlen("test");
-	printf("test: %i\n", test);
+	(void)av;
+	if (ac != 2)
+		return (prnt_err("Error: invalid argument count"));
 	mlx.ptr = mlx_init();
 	mlx.win = mlx_new_window(mlx.ptr, 1920, 1080, "Cub3D");
+	mlx_key_hook(mlx.win, key_event, &mlx);
+	mlx_hook(mlx.win, 17, 0L, close_window, &mlx);
 	mlx_loop(mlx.ptr);
 }
--- cub3d.h
+++ cub3d.h
@@ -6,18 +6,38 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:24:59 by lpeeters          #+#    #+#             */
-/*   Updated: 2024/03/21 21:49:44 by lpeeters         ###   ########.fr       */
+/*   Updated: 2024/03/26 19:13:29 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+/* LIBRARIES */
+
 # include "libft/libft.h" //Custom library functions
 # include "mlx/mlx.h" //Minilibx library functions
 # include "stdio.h" //printf
 
-//mlx data structure
+/* MACROS */
+
+# define SUCCESS 0
+# define FAILURE 1
+
+/* KEY CODES */
+
+# define ESCAPE 65307
+# define M 109
+# define UP 65362
+# define W 119
+# define LEFT 65361
+# define A 97
+# define DOWN 65364
+# define S 115
+# define RIGHT 65363
+# define D 100
+
+// MLX data structure
 typedef struct s_mlx
 {
 	void	*ptr;
